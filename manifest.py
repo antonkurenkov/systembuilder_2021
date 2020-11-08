3fc04e4047cf1314cc661623e68e79b1220c3cea
sobolev210 <68656754+sobolev210@users.noreply.github.com>
2020-11-08 13:19:10 +0300

    Add files via upload

---
+++ manifest.py
@@ -0,0 +1,24 @@
+import yaml
+
+
+class Manifest:
+
+    # структура конфигурационного файла manifest.yml
+    structure = ['name', 'description', 'author', 'url', 'version', 'license', 'keywords', 'path']
+
+    def __init__(self, path):
+        self.path = path
+
+    def validate(self):
+        with open(self.path) as file:
+            data = yaml.load(file, Loader=yaml.FullLoader)
+
+        # с помощью dict создаю копию словаря
+        data_dict = dict(data)
+        for key in data.keys():
+            if key not in self.structure:
+                del data_dict[key]
+        if len(data_dict) == len(self.structure):
+            return data_dict
+        else:
+            return None
