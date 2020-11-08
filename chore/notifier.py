c5f83df6fb2eb51ac581c736d100a6197447d7c2
dansobolev <daniil.sob56@gmail.com>
2020-11-08 14:24:21 +0300

    Added Parser and Notifier classes with certain methods

---
+++ chore/notifier.py
@@ -0,0 +1,19 @@
+class Parser:
+
+    def get_current_branch(self):
+        pass
+
+    def get_status(self):
+        pass
+
+
+class Notifier:
+
+    def __init__(self):
+        pass
+
+    def prepare(self):
+        pass
+
+    def send_message(self):
+        pass
