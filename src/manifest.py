4aebcf1c39812ae9941763f965b8545e84039608
sobolev210 <68656754+sobolev210@users.noreply.github.com>
2020-11-08 13:55:20 +0300

    Update manifest.py

--- src/manifest.py
+++ src/manifest.py
@@ -4,7 +4,7 @@ import yaml
 class Manifest:
 
     # структура конфигурационного файла manifest.yml
-    keywords = ['name', 'description', 'author', 'url', 'version', 'license', 'keywords', 'path']
+    structure = ['name', 'description', 'author', 'url', 'version', 'license', 'keywords', 'path']
 
     def __init__(self, path):
         self.path = path
@@ -16,9 +16,9 @@ class Manifest:
         # с помощью dict создаю копию словаря
         data_dict = dict(data)
         for key in data.keys():
-            if key not in self.keywords:
+            if key not in self.structure:
                 del data_dict[key]
-        if len(data_dict) == len(self.keywords):
+        if len(data_dict) == len(self.structure):
             return data_dict
         else:
             return None
