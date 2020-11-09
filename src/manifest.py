5bd159af2d3a69385e959c3e79a951356690aba1
Ivan Marchuk <ivan.marchuk@mail.ru>
2020-11-09 15:27:08 +0300

    created validate method

--- src/manifest.py
+++ src/manifest.py
@@ -3,8 +3,28 @@ import yaml
 
 class Manifest:
 
+    cfg_keys = ['name', 'description', 'author', 'url',
+                'version', 'license', 'keywords', 'path']
+
+    def __init__(self, path):
+        self.path = path
+
     def parse(self):
         pass
 
-    def load_file(self):
-        return yaml.safe_load(self.path)
+    def load_file(self, file):
+        return yaml.safe_load(file)
+
+    def validate(self):
+        with open(self.path, 'r') as stream:
+            try:
+                data = self.load_file(stream)
+                for key in data.keys():
+                    if key not in self.cfg_keys:
+                        del data[key]
+                if len(data) == len(self.cfg_keys):
+                    return data
+                else:
+                    return None
+            except yaml.YAMLError as ex:
+                print(ex)
