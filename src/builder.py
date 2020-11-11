54166555ca3fbe317b283d05057da798a7c49436
olexsahka <sutugin8@gmail.com>
2020-11-11 22:48:36 +0300

    update parse

--- src/builder.py
+++ src/builder.py
@@ -1,5 +1,15 @@
-class builder:
+import os.path
+
+
+class Builder:
+    def __init__(self, path):
+        self.path = path
+        self.collection = []
+        self.file = 'manifest.yml'
 
     def parse(self):
-        pass
-    
+        for root, dirs, files in os.walk(self.path):
+            if self.file in files:
+                self.collection.append(os.abspath.join(self.dirs))
+
+        return self.collection
