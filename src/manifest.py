eb72fd91272b68b852a5a1c3296e3c1eac059eb1
sobolev210 <68656754+sobolev210@users.noreply.github.com>
2020-11-21 13:51:57 +0300

    Update manifest.py

--- src/manifest.py
+++ src/manifest.py
@@ -9,16 +9,21 @@ class Manifest:
     def __init__(self, path):
         self.path = path
 
-    def validate(self):
-        with open(self.path) as file:
-            data = yaml.safe_load(file)
+    def load_file(self, file):
+        return yaml.safe_load(file)
 
-        # с помощью dict создаю копию словаря
-        data_dict = dict(data)
-        for key in data.keys():
-            if key not in self.structure:
-                del data_dict[key]
-        if len(data_dict) == len(self.structure):
-            return data_dict
-        else:
-            return None
+    def validate(self):
+        with open(self.path) as file_obj:
+            try:
+                data = self.load_file(file_obj)
+                # с помощью dict создаю копию словаря
+                data_dict = dict(data)
+                for key in data.keys():
+                    if key not in self.structure:
+                        del data_dict[key]
+                if len(data_dict) == len(self.structure):
+                    return data_dict
+                else:
+                    return None
+            except yaml.YAMLError as exclusion:
+                print(exclusion)
