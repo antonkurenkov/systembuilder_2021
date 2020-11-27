989c4d1394ede5cf13d25c2479ca312ca1727b81
Ivan Marchuk <ivan.marchuk@mail.ru>
2020-11-27 15:44:54 +0300

    feat: implemented main logic

--- src/builder.py
+++ src/builder.py
@@ -1,4 +1,7 @@
+import subprocess
 import os.path
+from manifest import Manifest
+
 
 class Builder:
 
@@ -6,6 +9,16 @@ class Builder:
         self.path = path
         self.collection = []
         self.file_name = 'manifest.yml'
+        self.status_map = None
+
+    def run(self):
+        self.parse()
+        for element in self.collection:
+            man = Manifest(element)
+            # returns yml structure
+            man.parse()
+            self.build()
+            self.update_status()
 
     def parse(self):
         for path, directories, files in os.walk(self.path):
@@ -14,5 +27,17 @@ class Builder:
 
         return self.collection
 
-    def build(self):
+    def update_status(self):
         pass
+
+    def build(self):
+        try:
+            self.update_status()
+            subprocess.run(['docker', 'buildx', 'build', self.path])
+        except Exception as error:
+            return error
+
+
+if __name__ == "__main__":
+    builder = Builder(".")
+    builder.run()
