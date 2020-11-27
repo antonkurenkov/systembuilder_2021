c6f426bc9a1d8205fb94a8bd4c13f3c9de806bac
VANADAIN <ussrivanko@gmail.com>
2020-11-27 18:01:11 +0300

    fix: rewrote functions

--- src/manifest.py
+++ src/manifest.py
@@ -3,29 +3,30 @@ import yaml
 class Manifest:
     def __init__ (self, path):
         self.path = path
-
-    cfg_keys = ['name', 'description', 'author', 'url',
+        self.structure
+        self.cfg_keys = ['name', 'description', 'author', 'url',
                 'version', 'license', 'keywords', 'path']
 
-    def __init__(self, path):
-        self.path = path
-
     def parse(self):
-        pass
-
-    def load_file(self, file):
-        return yaml.safe_load(file)
-
-    def validate(self):
+        load_file()
+        result = validate()
+        if (result):
+            return self.structure
+        else:
+            return None
+            
+    def load_file(self):
         with open(self.path, 'r') as stream:
             try:
-                data = self.load_file(stream)
-                for key in data.keys():
-                    if key not in self.cfg_keys:
-                        del data[key]
-                if len(data) == len(self.cfg_keys):
-                    return data
-                else:
-                    return None
-            except yaml.YAMLError as ex:
-                print(ex)
+                self.structure = yaml.safe_load(stream)
+            except Exception as e:
+                print(e)
+
+    def validate(self):
+        for key in self.structure.keys():
+            if key not in self.cfg_keys:
+                self.structure.pop(key)
+        if len(self.structure) == len(self.cfg_keys):
+            return True
+        else:
+            return False
