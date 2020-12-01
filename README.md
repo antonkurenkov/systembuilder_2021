ac3e252c532dd33f3d1e325b5f6956e9e4ce4716
olexsahka <sutugin8@gmail.com>
2020-12-01 04:17:34 +0300

    create def update_status(data,name,status),with json

--- README.md
+++
@@ -1,2 +0,0 @@
-# systembuilder_2021
-CI wrapper over github actions. Lanit-Tercom student projects 2021.
--- src/builder.py
+++ src/builder.py
@@ -1,4 +1,6 @@
 import os.path
+import json
+import subprocess
 
 
 class Builder:
@@ -6,6 +8,7 @@ class Builder:
         self.path = path
         self.collection = []
         self.file = 'manifest.yml'
+        self.status_map = []
 
     def parse(self):
         for root, dirs, files in os.walk(self.path):
@@ -13,3 +16,32 @@ class Builder:
                 self.collection.append(os.abspath.join(self.dirs))
 
         return self.collection
+
+    def update_status(self):
+        try:
+            with open("status (2).json", 'r') as status_file:
+                data_status = json.load(status_file)
+                for data in data_status.keys():
+                    self.status_map.append(data)
+                    for name in data_status[data].keys():
+                        self.status_map.append(name)
+                        for sym in data_status[data][name].keys():
+                            if sym == 'status':
+                                status = data_status[data][name][sym]
+                                self.status_map.append(status)
+            with open("README.md", 'r') as file:
+                for line in file:
+                    if 'v.' in line:
+                        version = eval(line[2:])
+                        self.status_map.append(version)
+            return self.status_map
+        except Exception as error:
+            return error
+
+    def build(self):
+        try:
+            self.update_status()
+            subprocess.run(['docker', 'buildx', 'build', self.path])
+        except Exception as error:
+            return error
+
